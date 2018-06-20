bde4083fbb49bcccec94f37d66fc200155b44566
695954085 <695954085@qq.com>
2018-06-20 16:59:49 +0800

    1. 添加自定义middleware(在页面没有改变的情况下，返回304)

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const Koa = require('koa');
-const serve = require('koa-static');
 const path = require('path');
 const compress = require('koa-compress');
+const serve = require('koa-static');
+const cache = require(path.join(__dirname,'src','middleware','koa-static-cache'));
 const app = new Koa();
 
 app.use(compress());
 
-app.use(serve(path.join(__dirname, 'public')))
+// 检查etag
+app.use(cache(path.join(__dirname, 'public')));
+
+app.use(serve(path.join(__dirname, 'public')));
+// app.use(koaStatic(path.join(__dirname,'public')));
 
 app.use(async (ctx) => {
   ctx.status = 404;
-  ctx.body = "404 NOT FOUND"
+  ctx.body = "404 NOT FOUND";
 })
 
 app.on('error', (err, ctx) => {
---
+++ src/middleware/koa-static-cache/index.js
@@ -0,0 +1,61 @@
+// 首先获取请求头，检查etag cache-control
+const fresh = require('fresh')
+const fs = require('mz/fs')
+const etag = require('etag')
+const {
+  parse, resolve
+} = require('path')
+/**
+ * Module exports.
+ */
+module.exports = exports = {}
+module.exports = cache
+
+function cache(root) {
+
+  return async function (ctx, next) {
+    let done = false;
+    if (ctx.method === 'GET' || ctx.method === 'HEAD') {
+      let path = ctx.path;
+      // 首先检查这个文件是否存在
+      let trailingSlash = path[path.length - 1] === '/'
+      let index = 'index.html'
+      path = path.substr(parse(path).root.length)
+      // index file support
+      if (index && trailingSlash) path += index
+      path = resolve(root, path)
+      try {
+        let stats = await fs.stat(path)
+        if (stats.isFile()) {
+          setHeader(ctx.res, stats)
+          if (isFresh(ctx.req, ctx.res)) {
+            notModified(ctx.res)
+            done = true;
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    if (!done) {
+      await next()
+    }
+  }
+}
+
+function notModified (res) {
+  res.statusCode = 304
+  res.end()
+}
+
+function isFresh(req, res) {
+  return fresh(req.headers, {
+    'etag': res.getHeader('ETag'),
+    'last-modified': res.getHeader('Last-Modified')
+  })
+}
+
+function setHeader(res, stats) {
+  res.setHeader('Last-Modified', stats.mtime.toUTCString())
+  res.setHeader('ETag', etag(stats))
+}
